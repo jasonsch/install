eea226e23108e84dd4e81eb764c59c578064cca9
Jason Schroeder <jasonsch@gmail.com>
2015-01-09 18:12:23 -0800

    Stash changes

---
+++ install.sh
@@ -0,0 +1,6 @@
+git config --global user.name "Jason Schroeder"
+git config --global user.email "jasonsch@gmail.com"
+git config --global push.default simple
+git config --global core.excludesfile ~/.gitignore_global
+
+mkdir ~/.bash_scripts
